b3b2697cba22a8cbf61ac1c8960aae1b7758a6c5
Nishit Patel <nishit.p1@ahduni.edu.in>
2025-04-12 17:17:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.querySelectorAll('[data-code]').forEach(function(button) {
-    button.addEventListener('click', function() {
-        // Get the id of the code block to copy from
-        const codeId = button.getAttribute('data-code');
-        const codeElem = document.getElementById(codeId);
-        if (codeElem) {
-            const code = codeElem.textContent;
-            navigator.clipboard.writeText(code);
-        } else {
-            alert('Code block not found!');
-        }
-    });
-});
+.
